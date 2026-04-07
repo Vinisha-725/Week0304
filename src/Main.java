5be0b7eaca183be1ec82d19ca5d101301aca5a1a
Vinisha-725 <vinisha.sahoo@gmail.com>
2026-04-07 15:19:34 +0530

    problem 5

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,40 @@
-import java.util.*;
-
-class Asset {
-    String name;
-    double returnRate;
-    double volatility;
-
-    Asset(String n, double r, double v) {
-        name = n; returnRate = r; volatility = v;
-    }
-
-    public String toString() {
-        return name + ":" + returnRate;
-    }
-}
-
 public class Main {
 
-    public static void mergeSort(List<Asset> list) {
-        list.sort(Comparator.comparingDouble(a -> a.returnRate));
+    // Linear Search
+    public static int linearSearch(String[] arr, String target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count++;
+            if (arr[i].equals(target)) {
+                System.out.println("Comparisons: " + count);
+                return i;
+            }
+        }
+        return -1;
     }
 
-    public static void quickSort(List<Asset> list) {
-        list.sort((a, b) -> {
-            if (b.returnRate != a.returnRate)
-                return Double.compare(b.returnRate, a.returnRate);
-            return Double.compare(a.volatility, b.volatility);
-        });
+    // Binary Search
+    public static int binarySearch(String[] arr, String target) {
+        int low = 0, high = arr.length - 1, count = 0;
+
+        while (low <= high) {
+            count++;
+            int mid = (low + high) / 2;
+            if (arr[mid].equals(target)) {
+                System.out.println("Comparisons: " + count);
+                return mid;
+            }
+            else if (arr[mid].compareTo(target) < 0)
+                low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        List<Asset> list = List.of(
-                new Asset("AAPL", 12, 5),
-                new Asset("TSLA", 8, 7),
-                new Asset("GOOG", 15, 4)
-        );
+        String[] arr = {"accA", "accB", "accB", "accC"};
 
-        List<Asset> copy1 = new ArrayList<>(list);
-        mergeSort(copy1);
-        System.out.println(copy1);
-
-        List<Asset> copy2 = new ArrayList<>(list);
-        quickSort(copy2);
-        System.out.println(copy2);
+        System.out.println("Linear: " + linearSearch(arr, "accB"));
+        System.out.println("Binary: " + binarySearch(arr, "accB"));
     }
 }
